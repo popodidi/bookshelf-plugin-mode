1cb3dc4c95a312934a888814272ab46a7ba81ca2
Hao <changhao@sendbest.cc>
2017-03-13 15:44:02 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import lodash from 'lodash';
 import BaseModel from './BaseModel';
 
 export default function (Bookshelf) {
-    Bookshelf.Model = BaseModel(Bookshelf)
+    // Bookshelf.Model = BaseModel(Bookshelf)
+    Bookshelf.Model = class extends Bookshelf.Model {
+        testPlugin() {
+            return "testPlugin"
+        }
+    }
 }

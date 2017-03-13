8081b6bd9381c0ee767e13e8a5c576a3e09b13e5
Hao <changhao@sendbest.cc>
2017-03-13 16:44:07 +0800

    test

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-exports.default = function (Bookshelf) {
-    Bookshelf.Model = (0, _BaseModel2.default)(Bookshelf);
-};
-
 var _lodash = require('lodash');
 
 var _lodash2 = _interopRequireDefault(_lodash);
@@ -17,5 +13,14 @@ var _BaseModel = require('./BaseModel');
 var _BaseModel2 = _interopRequireDefault(_BaseModel);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (bookshelf) {
+    // Bookshelf.Model = BaseModel(Bookshelf)
+    bookshelf.Model = bookshelf.Model.extend({
+        testPlugin: function testPlugin() {
+            return "testPlugintestestesteset";
+        }
+    });
+};
 
 module.exports = exports['default'];
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import BaseModel from './BaseModel';
 
 export default bookshelf => {
     // Bookshelf.Model = BaseModel(Bookshelf)
+    console.log("plugin ? ");
     bookshelf.Model = bookshelf.Model.extend({
         testPlugin() {
             return "testPlugintestestesteset"
         }
-    })
+    }
+    return bookshelf
 }

ff3ec902753ac33f97d1d3ecc12c36df33801bac
Hao <changhao@sendbest.cc>
2017-03-13 16:53:05 +0800

    test

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import BaseModel from './BaseModel';
 export default bookshelf => {
     // Bookshelf.Model = BaseModel(Bookshelf)
     console.log("plugin ? ");
-    // bookshelf.Model = bookshelf.Model.extend({
-    //     testPlugin() {
-    //         return "testPlugintestestesteset"
-    //     }
-    // })
-    bookshelf.Model = BaseModel(bookshelf)
+    bookshelf.Model = bookshelf.Model.extend({
+        testPlugin: function () {
+            return "testPlugintestestesteset"
+        }
+    })
+    // bookshelf.Model = BaseModel(bookshelf)
 }

b63ab8c8be93cb219e6d21d31bb1170168656190
Hao <changhao@sendbest.cc>
2017-03-14 09:52:28 +0800

    mode to json

--- dist/index.js
+++ dist/index.js
@@ -8,27 +8,53 @@ var _lodash = require('lodash');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
-var _BaseModel = require('./BaseModel');
-
-var _BaseModel2 = _interopRequireDefault(_BaseModel);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (bookshelf) {
-    var baseModel = (0, _BaseModel2.default)(bookshelf);
-    bookshelf.Model = baseModel;
-    bookshelf.Collection = bookshelf.Collection.extend({
-        model: baseModel
-    });
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-    console.log("pluginnnnnnnnnn collectionnnnn");
-    // console.log(bookshelf.Model);
-    // bookshelf.Model = bookshelf.Model.extend({
-    //     testPlugin: "testPlugintestestesteset"
-    // });
-    // bookshelf.Model.prototyp
-    // console.log(bookshelf.Model);
-    // console.log("pluginnnnnnnnnn");
+exports.default = function (bookshelf) {
+    var proto = bookshelf.Model.prototype;
+    var _toJSON = proto.toJSON;
+
+    bookshelf.Model = bookshelf.Model.extend({
+        modes: {
+            // "mode_1": {
+            //     visible: ['a', 'b'],
+            //     hidden : ['c', 'd']
+            // },
+            // "mode_2": {
+            //     visible: [],
+            //     hidden : []
+            // }
+        },
+        _mode: null,
+        mode: function mode(_mode) {
+            this._mode = _mode;
+            return this;
+        },
+        toJSON: function toJSON() {
+            var json = _toJSON.apply(this, arguments);
+
+            var visible, hidden;
+            if (!_.isNull(this._mode) && !_.isUndefined(_.get(this.modes, this._mode))) {
+                visible = _.get(this.modes, this._mode + '.visible');
+                hidden = _.get(this.modes, this._mode + '.hidden');
+            }
+
+            if (visible) {
+                var _ref;
+
+                json = (_ref = _).pick.apply(_ref, _toConsumableArray([json].concat(visible)));
+            }
+            if (hidden) {
+                var _ref2;
+
+                json = (_ref2 = _).omit.apply(_ref2, _toConsumableArray([json].concat(hidden)));
+            }
+
+            return json;
+        }
+    });
 };
 
 module.exports = exports['default'];
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import lodash from 'lodash';
 
 export default bookshelf => {
+    const proto = bookshelf.Model.prototype;
+    const toJSON = proto.toJSON;
+
     bookshelf.Model = bookshelf.Model.extend({
-        testPlugin: "testpluginnnnnnnnnnn"
-        // Ruby, put your code HERE!
+        modes : {
+            // "mode_1": {
+            //     visible: ['a', 'b'],
+            //     hidden : ['c', 'd']
+            // },
+            // "mode_2": {
+            //     visible: [],
+            //     hidden : []
+            // }
+        },
+        _mode : null,
+        mode  : function (mode) {
+            this._mode = mode;
+            return this;
+        },
+        toJSON: function () {
+            let json = toJSON.apply(this, arguments);
+
+            var visible, hidden;
+            if (!_.isNull(this._mode) && !_.isUndefined(_.get(this.modes, this._mode))) {
+                visible = _.get(this.modes, this._mode + '.visible');
+                hidden = _.get(this.modes, this._mode + '.hidden');
+            }
+
+            if (visible) {
+                json = _.pick(...[json].concat(visible));
+            }
+            if (hidden) {
+                json = _.omit(...[json].concat(hidden));
+            }
+
+            return json;
+        }
     });
 }
